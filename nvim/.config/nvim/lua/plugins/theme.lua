75c4168d1fab58700dddd9ff692ebaa652713d1c
phuong <aphuongu@gmail.com>
2026-06-07 22:13:40 +0700

    dacula dark.

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,21 +1,35 @@
 return {
-  "maxmx03/dracula.nvim", -- 🎯 CHUẨN PHOM Ở ĐÂY (Thêm số 3 vào đuôi)
+  "maxmx03/dracula.nvim",
   lazy = false,
   priority = 1000,
   config = function()
     require("dracula").setup({
       colors = {
-        bg = "#000000",          -- Ép nền đen xì nguyên khối đúng gu ông
-        bg_dark = "#000000",     
-        bg_float = "#000000",    
-        bg_sidebar = "#000000",  
+        bg = "#000000",
+        bg_dark = "#000000",
+        bg_float = "#000000",
+        bg_sidebar = "#000000",
         statusline_bg = "#000000",
       },
-      show_end_of_buffer = false, 
+      show_end_of_buffer = false,
       transparent_bg = false,
       lualine_bg_color = "#000000",
     })
     
-    vim.cmd[[colorscheme dracula]]
+    -- Kích hoạt theme gốc trước
+    vim.cmd([[colorscheme dracula]])
+
+    -- 🚀 ĐƯỜNG QUYỀN ĐÈ BẸP TRONG SUỐT: Chạy sau một nhịp để ép màu đen xì bằng lệnh lõi
+    vim.schedule(function()
+      local bg_den_tuyet_doi = { bg = "#000000", ctermbg = "black" }
+      local groups = { 
+        "Normal", "NormalNC", "SignColumn", "FoldColumn", 
+        "LineNr", "CursorLineNr", "StatusLine", "StatusLineNC",
+        "NeoTreeNormal", "NvimTreeNormal", "EndOfBuffer"
+      }
+      for _, group in ipairs(groups) do
+        vim.api.nvim_set_hl(0, group, bg_den_tuyet_doi)
+      end
+    end)
   end,
 }
