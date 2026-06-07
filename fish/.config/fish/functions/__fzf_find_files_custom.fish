9d1f451e0a570962bb777e830e1c2d3ca282b1f3
phuong <aphuongu@gmail.com>
2026-06-07 14:06:02 +0700

    absolute like pwd for file.

--- fish/.config/fish/functions/__fzf_find_files_custom.fish
+++ fish/.config/fish/functions/__fzf_find_files_custom.fish
@@ -6,9 +6,11 @@ function __fzf_find_files_custom
         --preview-window="bottom:50%" \
         --preview 'bat --style=numbers --color=always --line-range :100 {}')
     
-    # Nếu chọn được file, ném thẳng tên file đó vào vị trí con trỏ chuột trên Terminal
+    # Nếu chọn được file, dịch nó thành đường dẫn tuyệt đối bằng realpath
     if test -n "$file"
-        commandline -i (string escape $file)
+        set -l absolute_file (realpath -- $file)
+        # Ném thẳng tên file tuyệt đối đó vào vị trí con trỏ trên Terminal
+        commandline -i (string escape -- $absolute_file)
     end
     commandline -f repaint
 end
