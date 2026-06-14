c6db46bb0d886778762fb1765aac787d047102cf
phuong <aphuongu@gmail.com>
2026-06-14 22:37:57 +0700

    atuin true color and slstatus true color.

--- suckless/.suckless/slstatus/config.h
+++ suckless/.suckless/slstatus/config.h
@@ -65,12 +65,12 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 static const struct arg args[] = {
-    /* function     format                               argument */
-    { cpu_perc,     "  %s%%  │  ",                       NULL },
-    { ram_perc,     "  %s%%  │  ",                       NULL },
-    { run_command,  "󰃠  %s%%  │  ",                       "brightnessctl -m | cut -d, -f4 | tr -d '%'" },
-    { run_command,  "󰕾  %s%%  │  ",                       "amixer sget Master | grep -oE '[0-9]+%' | head -1 | tr -d '%'" },
-    { battery_perc, "󰁹 %s%% [",                         "BAT0" },
-    { battery_state, "%s]  │  ",                         "BAT0" },
-    { datetime,     "󰃭  %s",                             "%d/%m 󱑒 %H:%M " },
+    /* function         format                                      argument */
+    { cpu_perc,         "^c#FF5555^  %s%% ^d^ │  ",                NULL }, /* Text màu đỏ */
+    { ram_perc,         "^c#50FA7B^  %s%% ^d^ │  ",                NULL }, /* Text màu xanh lá (Dracula) */
+    { run_command,      "^c#F1FA8C^󰃠  %s%% ^d^ │  ",                "brightnessctl -m | cut -d, -f4 | tr -d '%'" },
+    { run_command,      "^c#BD93F9^󰕾  %s%% ^d^ │  ",                "amixer sget Master | grep -oE '[0-9]+%' | head -1 | tr -d '%'" },
+    { battery_perc,     "^c#FF79C6^󰁹 %s%% [",                       "BAT0" },
+    { battery_state,    "%s] ^d^ │  ",                              "BAT0" },
+    { datetime,         "^c#8BE9FD^󰃭  %s^d^",                        "%d/%m 󱑒 %H:%M " },
 };
