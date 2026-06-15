73058fa3117e4fe9ad28bcfdcbb680578d5640a7
phuong <aphuongu@gmail.com>
2026-06-15 15:26:27 +0700

    f1f2f3 to select.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -23,10 +23,10 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<CR>",    -- Bấm Enter để bắt đầu
-          node_incremental = "v",     -- Spam phím 'v' để bôi đen rộng ra (như cũ!)
-          scope_incremental = "grc",
-          node_decremental = "grm",   -- Bấm grm để thu hẹp lại
+          init_selection = "<F2>",       -- Bấm phím F2 để bắt đầu chọn vùng code
+          node_incremental = "<F3>",     -- Bấm phím F3 để bôi đen rộng ra từ từ (theo node)
+          scope_incremental = "S-v",     -- Bấm Shift + v để bôi đen nhanh toàn bộ hàm/khối code
+          node_decremental = "<F1>",     -- Bấm phím F1 để thu hẹp vùng chọn lại
         },
       },
     })
