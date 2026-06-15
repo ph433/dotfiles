6c19950c7f71afb6d1d228f998231b3b5a36f6d6
phuong <aphuongu@gmail.com>
2026-06-15 08:46:44 +0700

    zoom.

--- dwm_custom/dwm-flexipatch/config.h
+++ dwm_custom/dwm-flexipatch/config.h
@@ -1203,7 +1203,7 @@ static const Key keys[] = {
 	#if INSETS_PATCH
 	{ MODKEY|ShiftMask|ControlMask, XK_a,          updateinset,            {.v = &default_inset } },
 	#endif // INSETS_PATCH
-	{ MODKEY,                       XK_Return,     zoom,                   {0} },
+	{ MODKEY|ShiftMask,             XK_Return,     zoom,                   {0} },
 	#if VANITYGAPS_PATCH
 	{ MODKEY|Mod4Mask,              XK_u,          incrgaps,               {.i = +1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_u,          incrgaps,               {.i = -1 } },
