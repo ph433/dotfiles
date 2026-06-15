4392ade8342d0b977780bf4c58ca32f88ec48fb8
phuong <aphuongu@gmail.com>
2026-06-15 11:27:12 +0700

    ctrl shift left to choose the specific commit to restore.

--- fish/.config/fish/functions/gdiff.fish
+++ fish/.config/fish/functions/gdiff.fish
@@ -1,4 +1,4 @@
-function gdiff --description "FZF Git Diff All-in-One Dashboard (Tích hợp Dò Commit)"
+function gdiff --description "FZF Git Diff All-in-One Dashboard (Tích hợp gọi glog)"
     # Kiểm tra Git Repository
     git rev-parse --is-inside-work-tree >/dev/null 2>&1
     if test $status -ne 0
@@ -15,23 +15,38 @@ function gdiff --description "FZF Git Diff All-in-One Dashboard (Tích hợp Dò
     set -l unstage_cmd "fish -c 'for p in \$argv; if string match -q \"*->*\" \"\$p\"; git restore --staged (string split \" -> \" \"\$p\")[-1]; else; git restore --staged (string sub -s 4 \"\$p\"); end; end' -- {+}"
 
     # 2. Lệnh Hủy thay đổi (Ctrl-Left)
-    # Tự động phân biệt: Nếu là file Untracked (??) thì xóa hẳn (rm), nếu là Modified (M) thì git restore
     set -l discard_cmd "fish -c 'for p in \$argv; set code (string sub -l 2 \"\$p\"); set path \"\"; if string match -q \"*->*\" \"\$p\"; set path (string split \" -> \" \"\$p\")[-1]; else; set path (string sub -s 4 \"\$p\"); end; if test \"\$code\" = \"??\"; rm -rf \"\$path\"; else; git restore \"\$path\"; end; end' -- {+}"
 
-    # 3. Lệnh Dò Commit (Ctrl-T)
-    # Mở một FZF con lồng bên trong để xem git log, trích xuất Hash và restore file về thời điểm đó
+    # 3. Lệnh Dò Commit bằng cách gọi hàm glog (Ctrl-Shift-Left)
     set -l history_cmd "fish -c '
-        set selected_commit (git log --oneline --color=always | fzf --ansi --prompt=\" Chọn commit để khôi phục file: \" --preview=\"git show {1} --color=always\" < /dev/tty > /dev/tty)
-        if test -n \"\$selected_commit\"
-            set hash (string split -m 1 \" \" \"\$selected_commit\")[1]
-            for p in \$argv
-                set path \"\"
-                if string match -q \"*->*\" \"\$p\"
-                    set path (string split \" -> \" \"\$p\")[-1]
-                else
-                    set path (string sub -s 4 \"\$p\")
+        # [THỦ THUẬT] Tạo hàm commandline giả để bắt output từ glog
+        function commandline
+            if test \"\$argv[1]\" = \"--insert\"
+                echo \"\$argv[3]\" | string trim > /tmp/glog_hash
+            end
+        end
+
+        # Gọi hàm glog của bạn (đòi hỏi cấp lại luồng terminal để vẽ fzf)
+        glog </dev/tty >/dev/tty
+
+        # Xử lý kết quả trả về
+        if test -f /tmp/glog_hash
+            set hash (cat /tmp/glog_hash)
+            rm /tmp/glog_hash
+            
+            # Nếu glog trả về nhiều hash, chỉ lấy cái đầu tiên để restore
+            set hash (string split \" \" \"\$hash\")[1]
+
+            if test -n \"\$hash\"
+                for p in \$argv
+                    set path \"\"
+                    if string match -q \"*->*\" \"\$p\"
+                        set path (string split \" -> \" \"\$p\")[-1]
+                    else
+                        set path (string sub -s 4 \"\$p\")
+                    end
+                    git restore --source=\$hash \"\$path\"
                 end
-                git restore --source=\$hash \"\$path\"
             end
         end
     ' -- {+}"
@@ -48,13 +63,13 @@ function gdiff --description "FZF Git Diff All-in-One Dashboard (Tích hợp Dò
         --reverse \
         --tiebreak=index \
         --multi \
-        --header="[Git Diff]  Right: Add |  Left: Unstage |  C-Right: Commit |  C-Left: Bỏ thay đổi |  C-T: Dò Commit | ↵ Enter: Full" \
+        --header="[Git Diff]  Right: Add |  Left: Unstage |  C-Right: Commit |  C-Left: Hủy thay đổi |  C-S-Left: Gọi glog | ↵ Enter: Full" \
         --preview='if test {1} = "??"; bat --color=always --style=numbers -- {-1} 2>/dev/null || cat {-1}; else; git diff HEAD --color=always -- {-1} | delta --width=$FZF_PREVIEW_COLUMNS; end' \
         --preview-window="bottom:70%,border-top" \
         --bind="right:execute-silent($add_cmd)+reload(git -c color.status=always status -s)" \
         --bind="left:execute-silent($unstage_cmd)+reload(git -c color.status=always status -s)" \
         --bind="ctrl-left:execute-silent($discard_cmd)+reload(git -c color.status=always status -s)" \
-        --bind="ctrl-t:execute($history_cmd)+reload(git -c color.status=always status -s)" \
+        --bind="ctrl-shift-left:execute($history_cmd)+reload(git -c color.status=always status -s)" \
         --bind="enter:execute($full_screen_cmd)" \
         --expect=ctrl-right)
 
