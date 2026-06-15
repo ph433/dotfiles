c190e0eb74704615ffea5d843664e78c01802ba2
phuong <aphuongu@gmail.com>
2026-06-15 09:19:45 +0700

    zoom.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,22 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+
   config = function()
-    -- Bật tính năng căn lề mặc định của Neovim
     vim.opt.smartindent = true
 
-    -- Gọi trực tiếp hàm setup của nvim-treesitter bản mới
-    require("nvim-treesitter").setup({
-      ensure_installed = { "fish", "toml", "lua", "vim", "vimdoc", "markdown", "bash" },
-      auto_install = true,
+    -- 1. Cài đặt các parser ngôn ngữ (thay thế cho ensure_installed)
+    require("nvim-treesitter").install({
+      "fish", "toml", "lua", "vim", "vimdoc", "markdown", "bash", "commonlisp"
+    })
+
+    -- 2. BẬT HIGHLIGHT: Kích hoạt Treesitter bằng API gốc của Neovim (thay thế cho highlight = true)
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("treesitter_highlight", { clear = true }),
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
     })
   end,
 }
