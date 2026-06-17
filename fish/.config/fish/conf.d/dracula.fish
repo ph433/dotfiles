55961135bf6fb7dfb001514a4dc96cc5e68538f3
phuong <aphuongu@gmail.com>
2026-06-17 13:46:08 +0700

    fish... color of dollar.

---
+++ fish/.config/fish/conf.d/dracula.fish
@@ -0,0 +1,25 @@
+# ~/dotfiles/fish/.config/fish/conf.d/dracula.fish
+# Module cấu hình màu Dracula True Color cho Fish Shell (Chuẩn Stow/Dotfiles)
+
+set -g fish_color_normal F8F8F2
+set -g fish_color_command 50FA7B --bold
+set -g fish_color_keyword FF79C6 --bold
+set -g fish_color_quote F1FA8C
+set -g fish_color_redirection 8BE9FD
+set -g fish_color_end 50FA7B
+set -g fish_color_error FF5555
+set -g fish_color_param FFB86C
+set -g fish_color_comment 6272A4
+set -g fish_color_match --background=6272A4
+set -g fish_color_selection WHITE --bold --background=44475A
+set -g fish_color_search_match mulberry --background=44475A
+set -g fish_color_history_current --bold
+set -g fish_color_operator FFB86C --bold
+set -g fish_color_escape FF79C6
+set -g fish_color_cwd 50FA7B
+set -g fish_color_cwd_root FF5555
+set -g fish_color_valid_path --underline
+set -g fish_color_autosuggestion 6272A4
+set -g fish_color_user 8BE9FD
+set -g fish_color_host 50FA7B
+set -g fish_color_cancel FF5555 --reverse
