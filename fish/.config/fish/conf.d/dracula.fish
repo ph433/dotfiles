defd1705d9bdf31d1f0cfca6abf3a8972030bc99
phuong <aphuongu@gmail.com>
2026-06-18 13:14:30 +0700

    modified shortcuts

--- fish/.config/fish/conf.d/dracula.fish
+++ fish/.config/fish/conf.d/dracula.fish
@@ -1,5 +1,5 @@
 # ~/dotfiles/fish/.config/fish/conf.d/dracula.fish
-# Module cấu hình màu Dracula True Color cho Fish Shell (Chuẩn Stow/Dotfiles)
+# Module cấu hình màu Dracula True Color cho Fish Shell (Đã đồng bộ 100%)
 
 set -g fish_color_normal F8F8F2
 set -g fish_color_command 50FA7B --bold
@@ -8,13 +8,13 @@ set -g fish_color_quote F1FA8C
 set -g fish_color_redirection 8BE9FD
 set -g fish_color_end 50FA7B
 set -g fish_color_error FF5555
-set -g fish_color_param FFB86C
+set -g fish_color_param 8BE9FD              # Đã sửa về 8BE9FD chuẩn theo thực tế của bạn
 set -g fish_color_comment 6272A4
 set -g fish_color_match --background=6272A4
 set -g fish_color_selection WHITE --bold --background=44475A
-set -g fish_color_search_match mulberry --background=44475A
+set -g fish_color_search_match 44475A --background=44475A
 set -g fish_color_history_current --bold
-set -g fish_color_operator FFB86C --bold
+set -g fish_color_operator BD93F9 --bold
 set -g fish_color_escape FF79C6
 set -g fish_color_cwd 50FA7B
 set -g fish_color_cwd_root FF5555
@@ -22,4 +22,6 @@ set -g fish_color_valid_path --underline
 set -g fish_color_autosuggestion 6272A4
 set -g fish_color_user 8BE9FD
 set -g fish_color_host 50FA7B
+set -g fish_color_host_remote F1FA8C        # Thêm vào để đè bẹp 'yellow' cũ
+set -g fish_color_status FF5555             # Thêm vào để đè bẹp 'red' cũ
 set -g fish_color_cancel FF5555 --reverse
