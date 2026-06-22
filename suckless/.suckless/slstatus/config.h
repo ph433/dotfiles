1f38ecc89a8262a0a8ea256e32044ca014330052
phuong <aphuongu@gmail.com>
2026-06-22 15:34:56 +0700

    icon audio

--- suckless/.suckless/slstatus/config.h
+++ suckless/.suckless/slstatus/config.h
@@ -69,7 +69,7 @@ static const struct arg args[] = {
     { cpu_perc,         "^c#FF5555^󰻠  %s%% ^d^  ",                 NULL }, /* Text màu đỏ */
     { ram_perc,         "^c#50FA7B^  %s%% ^d^  ",                 NULL }, /* Text màu xanh lá (Dracula) */
     { run_command,      "^c#F1FA8C^󰃠  %s%% ^d^  ",                 "brightnessctl -m | cut -d, -f4 | tr -d '%'" },
-    { run_command,      "^c#BD93F9^󰕾  %s%% ^d^  ",                 "amixer sget Master | grep -oE '[0-9]+%' | head -1 | tr -d '%'" },
+    { run_command,      "^c#BD93F9^%s ^d^ ",                 "status=$(wpctl get-volume @DEFAULT_AUDIO_SINK@); vol=$(echo \"$status\" | awk -F'.' '{print $2}' | sed 's/^0//'); if echo \"$status\" | /usr/bin/grep -q '\\[MUTED\\]'; then echo \"󰝟 MUTE\"; else if pactl list sinks 2>/dev/null | /usr/bin/grep -q 'Active Port: analog-output-headphones'; then echo \"󰋋 ${vol}%\"; else echo \"󰕾 ${vol}%\"; fi; fi" },
     { battery_perc,     "^c#FF79C6^󰁹 %s%% [",                       "BAT0" },
     { battery_state,    "%s] ^d^  ",                                "BAT0" },
     { datetime,         "^c#8BE9FD^  %s^d^",                        "%d/%m 󰥔 %H:%M " },
