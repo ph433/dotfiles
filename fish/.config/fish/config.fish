4aa1ecd341fde5aba91cfe114639257153fdc6fd
phuong <aphuongu@gmail.com>
2026-06-30 19:04:04 +0700

    o1 sped

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -72,11 +72,94 @@ function _enter_or_fzf_zoxide
     end
 end
 
+# Xử lý cho phím Home
+function _home_or_fzf_search_dir
+    set -l current_cmd (commandline | string trim)
+    if test -z "$current_cmd"
+        __fzf_find_files_custom
+        commandline -f repaint
+    else
+        commandline -f beginning-of-line
+    end
+end
+
+# Xử lý cho phím End
+function _end_or_fzf_find
+    set -l current_cmd (commandline | string trim)
+    if test -z "$current_cmd"
+        __fzf_search_directory_custom
+        commandline -f repaint
+    else
+        commandline -f end-of-line
+    end
+end
+
+# Xử lý cho phím Space (Khoảng trắng)
+function _space_or_fzf_recent
+    set -l current_cmd (commandline | string trim)
+    if test -z "$current_cmd"
+        __fzf_file_recent
+        commandline -f repaint
+    else
+        # Thêm dấu cách bình thường nếu dòng lệnh đang có chữ
+        commandline -i ' '
+    end
+end
+
+# Xử lý cho phím Esc
+function _esc_or_become_ll
+    set -l current_cmd (commandline | string trim)
+    if test -z "$current_cmd"
+        # Thay thế dòng lệnh bằng 'll' và thực thi ngay lập tức
+        commandline -r 'll'
+        commandline -f execute
+    else
+        # Nếu dòng lệnh đang có chữ, giữ nguyên hành vi hủy/thoát mặc định của Esc
+        commandline -f cancel
+    end
+end
+
+
+# Xử lý cho phím Esc
+function _esc_or_become_lta
+    set -l current_cmd (commandline | string trim)
+    if test -z "$current_cmd"
+        # Thay thế dòng lệnh bằng 'll' và thực thi ngay lập tức
+        commandline -r 'lta'
+        commandline -f execute
+    else
+        # Nếu dòng lệnh đang có chữ, giữ nguyên hành vi hủy/thoát mặc định của Esc
+        commandline -f cancel
+    end
+end
+
+# Xử lý nâng cao cho Backspace (hoặc Ctrl+Z)
+function _backspace_toggle_home
+    set -l current_cmd (commandline | string trim)
+    if test -z "$current_cmd"
+        # Kiểm tra xem thư mục hiện tại có phải là Home hay không
+        if test "$PWD" = "$HOME"
+            # Nếu đang ở Home, quay lại thư mục trước đó (tương đương cd -)
+            cd -
+        else
+            # Nếu đang ở thư mục khác, đi về Home
+            cd ~
+        end
+        commandline -f repaint
+    else
+        # Nếu đang có chữ, xóa ký tự như bình thường
+        commandline -f backward-delete-char
+    end
+end
+
 function fish_user_key_bindings
-    # Gán phím Enter
     bind \r _enter_or_fzf_zoxide
-    
-    # Gán phím Right (\e[C) và Left (\e[D)
     bind \e\[C _right_or_fzf_recent
     bind \e\[D _left_or_fzf_find
+    bind \e\[H _home_or_fzf_search_dir
+    bind \e\[F _end_or_fzf_find
+    bind ' ' _space_or_fzf_recent
+    bind \e _esc_or_become_ll
+    bind \x7f _backspace_toggle_home
+    bind \e\[3~ _esc_or_become_lta
 end
