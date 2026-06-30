1315fa60e55e5942c610a3137b369ed751a2e985
phuong <aphuongu@gmail.com>
2026-06-30 16:14:29 +0700

    peak binding

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -37,3 +37,46 @@ function __log_recent_dir --on-variable PWD
     set -e __is_logging_dir
 end
 
+# Xử lý cho phím mũi tên Phải (Right Arrow)
+function _right_or_fzf_recent
+    set -l current_cmd (commandline | string trim)
+    if test -z "$current_cmd"
+        fzfrecentdir
+        commandline -f repaint
+    else
+        # Hành vi mặc định: di chuyển con trỏ sang phải
+        commandline -f forward-char
+    end
+end
+
+# Xử lý cho phím mũi tên Trái (Left Arrow)
+function _left_or_fzf_find
+    set -l current_cmd (commandline | string trim)
+    if test -z "$current_cmd"
+        fzfrecent
+        commandline -f repaint
+    else
+        # Hành vi mặc định: di chuyển con trỏ sang trái
+        commandline -f backward-char
+    end
+end
+
+# Xử lý cho phím Enter (đã làm ở bước trước)
+function _enter_or_fzf_zoxide
+    set -l current_cmd (commandline | string trim)
+    if test -z "$current_cmd"
+        __fzf_zoxide_custom
+        commandline -f repaint
+    else
+        commandline -f execute
+    end
+end
+
+function fish_user_key_bindings
+    # Gán phím Enter
+    bind \r _enter_or_fzf_zoxide
+    
+    # Gán phím Right (\e[C) và Left (\e[D)
+    bind \e\[C _right_or_fzf_recent
+    bind \e\[D _left_or_fzf_find
+end
