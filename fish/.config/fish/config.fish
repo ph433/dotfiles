b372d38952474b106b87299a0cc1b46188cc51e9
phuong <aphuongu@gmail.com>
2026-07-10 21:25:27 +0700

    file enter right off the bat.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -191,10 +191,34 @@ function _down_fzf_recent_or_menu
     end
 end
 
+function __nvim_open_latest_recent
+    set -l log_file "$HOME/.cache/nvim_recent.log"
+    
+    if test -f $log_file
+        # Sắp xếp cột 1 (timestamp) giảm dần, lấy dòng đầu tiên, và cắt bỏ cột timestamp để lấy đường dẫn file
+        set -l latest_file (sort -rn $log_file | head -n 1 | string replace -r '^\d+\s+' '')
+
+        if test -n "$latest_file"; and test -f $latest_file
+            # Xóa dòng hiện tại trên terminal trước khi mở nvim để tránh rác lệnh
+            commandline -r ""
+            nvim $latest_file
+            # Ép Fish vẽ lại prompt sau khi thoát nvim
+            commandline -f repaint
+        else
+            echo -e "\n[Lỗi] File không tồn tại hoặc đường dẫn trống: $latest_file"
+            commandline -f repaint
+        end
+    else
+        echo -e "\n[Lỗi] Không tìm thấy file log: $log_file"
+        commandline -f repaint
+    end
+end
+
 function fish_user_key_bindings
     bind enter _enter_or_fzf_zoxide
     bind down "__fish_exec_in_main_mode_only down-line _down_fzf_recent_or_menu"
     bind right _right_or_fzf_recent
+    bind insert __nvim_open_latest_recent
     bind left _left_or_fzf_find
     bind home _home_or_fzf_search_dir
     bind end _end_or_fzf_find
