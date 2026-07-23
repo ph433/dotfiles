781504a1c84922b2724405f4d79b6fa49891f681
phuong <aphuongu@gmail.com>
2026-07-23 22:27:34 +0700

    layer ala, firefox

--- dwm_custom/dwm-flexipatch/patch/kanata.c
+++ dwm_custom/dwm-flexipatch/patch/kanata.c
@@ -46,6 +46,10 @@ update_kanata_layer(Client *c) {
 
     if (ch.res_class && strstr(ch.res_class, "qutebrowser")) {
         set_kanata_layer("mod_qutebrowser");
+    } else if (ch.res_class && strcasestr(ch.res_class, "firefox")) {
+        set_kanata_layer("mod_firefox");
+    } else if (ch.res_class && strcasestr(ch.res_class, "Alacritty")) {
+        set_kanata_layer("mod_nvim");
     } else {
         set_kanata_layer("mod_nvim");
     }
