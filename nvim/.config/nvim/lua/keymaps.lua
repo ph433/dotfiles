065237477e3bf4bb9123be6ad1eed93aa1ad90d8
phuong <aphuongu@gmail.com>
2026-08-18 22:55:48 +0700

    ent

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -175,9 +175,27 @@ vim.keymap.set('n', 'O', function()
 end, { desc = "Thay thế dòng giữ nguyên thụt lề" })
 
 vim.keymap.set('n', '<CR>', function()
-	  local keys = vim.api.nvim_replace_termcodes('a<CR> <BS><Esc>', true, false, true)
-	    vim.api.nvim_feedkeys(keys, 'n', false)
-    end, { desc = 'Ngắt dòng sau con trỏ và giữ vị trí thụt lề' })
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line = vim.api.nvim_get_current_line()
+  local line_len = #line
+  local indent = line:match('^(%s*)') or ''
+
+  if line_len == 0 or col >= line_len - 1 then
+    -- Đang ở cuối dòng (giống 'a' + Enter):
+    -- Thêm dòng mới có sẵn khoảng indent và chuyển con trỏ xuống đúng vị trí đó
+    vim.api.nvim_buf_set_lines(0, row, row, false, { indent })
+    vim.api.nvim_win_set_cursor(0, { row + 1, #indent })
+  else
+    -- Đang ở giữa dòng (giống 'i' + Enter):
+    -- Ngắt từ trước ký tự con trỏ đang đứng, đẩy toàn bộ phần sau xuống dòng kèm indent
+    local before = line:sub(1, col)
+    local after = line:sub(col + 1)
+
+    vim.api.nvim_set_current_line(before)
+    vim.api.nvim_buf_set_lines(0, row, row, false, { indent .. after })
+    vim.api.nvim_win_set_cursor(0, { row + 1, #indent })
+  end
+end, { desc = 'Ngắt dòng: theo sau con trỏ và giữ nguyên indent' })
 
 vim.keymap.set('n', '<S-CR>', function()
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
