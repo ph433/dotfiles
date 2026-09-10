32a446801959c90959044f6d94b2b16f50c5c141
phuong <aphuongu@gmail.com>
2026-09-10 14:04:43 +0700

    fish -- this is vi mod

--- fish/.config/fish/conf.d/tools.fish
+++ fish/.config/fish/conf.d/tools.fish
@@ -4,6 +4,12 @@ set -x BAT_THEME "Dracula"
 set -g fish_greeting ""
 set -x MY_QUICK_DIR
 set -gx MY_QUICK_DIR ~/dotfiles
+set -gx FZF_DEFAULT_OPTS "
+  --prompt='NORMAL > ' \
+  --bind='b:accept,j:down,k:up,q:abort' \
+  --bind='a:unbind(a,b,j,k,q)+change-prompt(INSERT > )' \
+  --bind='esc:transform:[ \"\$FZF_PROMPT\" = \"INSERT > \" ] && echo \"rebind(a,b,j,k,q)+change-prompt(NORMAL > )\" || echo \"abort\"'
+"
 # set -g fish_key_bindings fish_vi_key_bindings
 # source ~/.config/fish/myshortcuts.fish
 # set -x LS_COLORS "$LS_COLORS:.ICEAuthority=38;2;255;184;108:"
